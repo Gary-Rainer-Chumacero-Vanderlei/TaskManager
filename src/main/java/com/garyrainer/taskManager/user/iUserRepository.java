6031fe07a7a10a6c4aa31ab471fdfd4b21a612ca
Gary Rainer <garyvanderlei@gmail.com>
2025-10-02 16:01:09 -0300

    user creation

---
+++ src/main/java/com/garyrainer/taskManager/user/iUserRepository.java
@@ -0,0 +1,11 @@
+package com.garyrainer.taskManager.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface iUserRepository extends JpaRepository<UserModel, UUID>{
+  
+  UserModel findByUsername(String username);
+
+}
