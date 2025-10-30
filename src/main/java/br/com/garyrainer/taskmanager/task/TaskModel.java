182b933b473e407e57eb55afc74356018ffada98
Gary Rainer <garyvanderlei@gmail.com>
2025-10-30 13:31:54 -0300

    task, filter, validation

--- src/main/java/br/com/garyrainer/taskmanager/task/TaskModel.java
+++ src/main/java/br/com/garyrainer/taskmanager/task/TaskModel.java
@@ -1,4 +1,4 @@
-package br.com.garyrainer.taskmanager.user;
+package br.com.garyrainer.taskmanager.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,18 +12,23 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name = "tb_users")
-public class UserModel {
+@Entity(name = "tb_tasks")
+public class TaskModel {
 
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
+ 
+  @Column(length = 50)
+  private String title;
+  private String description;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
 
-  @Column(unique = true)
-  private String name;
-  private String username;
-  private String password;
-  
   @CreationTimestamp
   private LocalDateTime createdAt;
+
+  private UUID idUser;
+  
 }
